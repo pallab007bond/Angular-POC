fab8cd24adc7f84b5928cebc613fcee4ca6f0c34
pallab banerjee <rik.pallab@gmail.com>
2018-12-31 23:32:39 +0530

    ng for for change detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,15 @@ export class AppComponent
    courses=[
      {id: 1,name:'Course1'},
      {id: 2,name:'Course2'},
-     {id: 3,name:'Course3'},
+     {id: 3,name:'Course3'}
    ]
+   onAdd()
+   {
+      this.courses.push({id: 4,name:'Course4'});
+   }
+   onRemove(course)
+   {
+      let index= this.courses.indexOf(course);
+      this.courses.splice(index,1);
+   }
 }
